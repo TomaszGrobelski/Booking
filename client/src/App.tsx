51ac9bfbd9a0026e67cbeba902abf5726ebff7ee
Tomasz <bleniog@interia.pl>
2024-01-03 19:44:43 +0100

    Initial commit: Create directory structure

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div>
+        <div className="m-4 h-2 hover:text-[2px] text-red-500 ">dsad</div>
+        <div className="text-[40px]"> cldasdsa</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
